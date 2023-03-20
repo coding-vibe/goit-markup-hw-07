856c28dd0b677055fb61692cc7b5eb75dc137f99
merry <k.maria.m.24.05@gmail.com>
2023-03-20 07:30:54 +0200

    Add HW7

---
+++ js/mobile-menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", togglemenu);
+  refs.closeMenuBtn.addEventListener("click", togglemenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
